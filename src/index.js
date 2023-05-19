e87d2b89fae82f808dd5d635628817377ac508df
Ignacio <naxete7@hotmail.es>
2023-05-19 17:26:42 +0200

    resolve bug

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* Rodeo a la app del provider */}
-
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>
